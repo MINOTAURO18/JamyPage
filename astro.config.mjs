b01e9f58ea5db54e2b95edd01496a8712aac32b4
alexander muñoz <alexander12345my@gmail.com>
2023-09-21 18:57:04 -0500

    footer 70%

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   ViewTransitions: true,
   output: "server",
   adapter: vercel(),
-  integrations: [tailwind()]
+  integrations: [tailwind(), react()]
 });
