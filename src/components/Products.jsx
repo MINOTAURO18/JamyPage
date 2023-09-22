5d2ace46f9064ab10c0126260bb7e5fa5e5c8974
alexander muñoz <alexander12345my@gmail.com>
2023-09-21 20:35:26 -0500

    start with react

---
+++ src/components/Products.jsx
@@ -0,0 +1,9 @@
+
+
+const Products = () => {
+    return(
+        <p className="text-white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, facere odit quia labore porro nisi consectetur numquam. Blanditiis repudiandae ipsam voluptatum iste autem libero facere quam in. Dignissimos, impedit aut?</p>
+    )
+}
+
+export default Products;
